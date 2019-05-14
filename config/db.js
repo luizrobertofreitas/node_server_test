a10745314adb37e042b975e17de67938f922c186
Luiz Roberto Freitas <luiz.junior@accurate.com.br>
2019-05-14 13:37:30 -0300

    Configuring mongo url

--- config/db.js
+++ config/db.js
@@ -1,3 +1,13 @@
+
+/*
+	Username: node_server_admin
+	Password: 123456
+	Database Name: node_server_test
+	Connection URL: mongodb://node_server_admin:123456@mongodb/node_server_test
+*/
+
 module.exports = {
-	url : "mongodb://localhost:27017/node_server_test"
+	//url : "mongodb://localhost:27017/node_server_test"
+	url : "mongodb://node_server_admin:123456@mongodb/node_server_test"
+	// url : "mongodb://node_server_admin:123456@mongodb-my-demo-project.1d35.starter-us-east-1.openshiftapps.com:27017/node_server_test"
 }
